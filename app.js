b619c83df1cf2f52474a91ac354e742a4d3c7f0b
aaa <a@a.com>
2016-02-11 17:15:50 +0100

    general updates

--- app.js
+++ app.js
@@ -1,42 +1,32 @@
 var io = require('socket.io-client');
 var dgram = require('dgram');
 
-
-var serverUrl = 'http://localhost:3000';
+var serverUrl = 'http://151.80.177.19:3000';
 var socket = io.connect(serverUrl);
 
-//var p1 = 'hello';
-//socket.emit('call', p1, function(resp, data) {
-//    console.log('server sent resp code ' + resp);
-//});
-
-var host = "127.0.0.1";
+var host = '151.80.177.19';
 var port = 12345;
-var bindip = '127.0.0.1';
 var bindport = '1000';
 var client = dgram.createSocket( "udp4" );
-client.bind(bindport, host);
+
+client.bind(bindport, '0.0.0.0');
 
 console.log('Starting socket.io client...');
 socket.on('connect', function () {
     console.log("socket connected");
-    socket.emit('register', { name: 'alex', email: 'alexdw1@gmail.com', api: 'abc', port: '12345' });
-
-
+    socket.emit('register', { name: 'alex', email: 'alexdw1@github.com', api: 'abc', port: '12345' });
 });
 socket.on('udpfire', function(msg){
     console.log('udpfire: ' + msg.port);
-    //sendudp.log('')
+    sendudp(host, msg.port);
 });
 
 setInterval(function() {
-    console.log('ping');
     socket.emit('keepalive', '');
     sendudp(host, port);
 }, 2000);
 
 function sendudp(host, port) {
-    var message = new Buffer( "My KungFu is Good!" );
+    var message = new Buffer( "random" );
     client.send(message, 0, message.length, port, host );
-    return;
 }
